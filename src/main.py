af056ac167000f0cc34878ed387bc41132fbfb2e
gustavo-hsm <gustavohsm100@gmail.com>
2020-06-29 12:30:44 -0300

    Initial draft of a threading mechanism

--- src/main.py
+++ src/main.py
@@ -1,28 +1,44 @@
 from ExchangeItem import ExchangeItem
 from RequestHandler import PostHandler, GetHandler
 
+import threading as th
 from json import dump
 from datetime import datetime
 from time import sleep
 
 
+# TODO: Enable stop conditions, such as:
+# Request N times
+# Request until datetime
 def stop_condition():
     return True
 
 
-def collect_data(poster, getter, ExchangeItem):
-    print(ExchangeItem.json_params)
-    # Ensure all entities are subscribed to one another
-    poster.subscribe(getter)
-    getter.subscribe(ExchangeItem)
+# TODO: Create an specialized class to handle collecting data
+def collect_data(*args):
+    exchange = args[0]
+    league = exchange.get_league()
+    while stop_condition():
+        # Debug
+        print(exchange.json_params)
 
-    # Send and process requests
-    poster.send_request()
-    getter.send_request()
+        # Create handlers
+        poster = PostHandler(exchange)
+        getter = GetHandler(exchange)
+
+        # Attach subscribers to publishers
+        poster.subscribe(getter)
+        getter.subscribe(exchange)
+
+        # Run
+        poster.send_request()
+        getter.send_request()
+
+        # Timeout before running again
+        sleep(45)
 
 
-league = 'harvest'
-
+# * * Sample Requests * *
 # Topics to track
 exalt_to_mirror = ExchangeItem(want='mir', have='exalt')
 chaos_to_exalt = ExchangeItem(want='exalt', have='chaos')
@@ -31,14 +47,17 @@ chaos_to_common = ExchangeItem(want=['alt', 'fuse', 'ancient_orb'],
 common_to_common = ExchangeItem(want=['fuse', 'ancient_orb'],
                                 have=['chaos', 'alt'])
 
-# Request Handlers
-chaos_to_exalt_poster = PostHandler(chaos_to_exalt, league)
-chaos_to_exalt_getter = GetHandler(chaos_to_exalt, league)
+# Assign a new thread for each topic
+thread1 = th.Thread(target=collect_data, args=(exalt_to_mirror,))
+thread2 = th.Thread(target=collect_data, args=(chaos_to_exalt,))
+thread3 = th.Thread(target=collect_data, args=(chaos_to_common,))
+thread4 = th.Thread(target=collect_data, args=(common_to_common,))
 
-
-while stop_condition():
-    collect_data(chaos_to_exalt_poster, chaos_to_exalt_getter, chaos_to_exalt)
-
-    # Wait 2 minutes before trying again
-    print('Processing is done. Waiting...')
-    sleep(45)
+# Start collecting
+thread1.start()
+sleep(5)
+thread2.start()
+sleep(5)
+thread3.start()
+sleep(5)
+thread4.start()
