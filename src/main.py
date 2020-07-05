0f924ed9310a410aa02b882ef9803d21f5798a7d
gustavo-hsm <gustavohsm100@gmail.com>
2020-07-05 17:40:08 -0300

    Refactored main file using Request Negotiator

--- src/main.py
+++ src/main.py
@@ -1,63 +1,18 @@
 from ExchangeItem import ExchangeItem
-from RequestHandler import PostHandler, GetHandler
+from RequestNegotiator import RequestNegotiator
 
-import threading as th
-from json import dump
-from datetime import datetime
-from time import sleep
+# Request negotiator
+negotiator = RequestNegotiator()
 
-
-# TODO: Enable stop conditions, such as:
-# Request N times
-# Request until datetime
-def stop_condition():
-    return True
-
-
-# TODO: Create an specialized class to handle collecting data
-def collect_data(*args):
-    exchange = args[0]
-    league = exchange.get_league()
-    while stop_condition():
-        # Debug
-        print(exchange.json_params)
-
-        # Create handlers
-        poster = PostHandler(exchange)
-        getter = GetHandler(exchange)
-
-        # Attach subscribers to publishers
-        poster.subscribe(getter)
-        getter.subscribe(exchange)
-
-        # Run
-        poster.send_request()
-        getter.send_request()
-
-        # Timeout before running again
-        sleep(45)
-
-
-# * * Sample Requests * *
 # Topics to track
-exalt_to_mirror = ExchangeItem(want='mir', have='exalt')
-chaos_to_exalt = ExchangeItem(want='exalt', have='chaos')
-chaos_to_common = ExchangeItem(want=['alt', 'fuse', 'ancient_orb'],
-                               have='chaos')
-common_to_common = ExchangeItem(want=['fuse', 'ancient_orb'],
-                                have=['chaos', 'alt'])
+exalt_to_mirror = ExchangeItem(want='mirror', have='exalted')
+high_stock_exalt_to_mirror = ExchangeItem(want='mirror', have='exalted',
+                                          minimum_stock=10,
+                                          allow_adjust_minimum_stock=False)
 
-# Assign a new thread for each topic
-thread1 = th.Thread(target=collect_data, args=(exalt_to_mirror,))
-thread2 = th.Thread(target=collect_data, args=(chaos_to_exalt,))
-thread3 = th.Thread(target=collect_data, args=(chaos_to_common,))
-thread4 = th.Thread(target=collect_data, args=(common_to_common,))
+# Attach topics
+negotiator.add_topic(exalt_to_mirror)
+negotiator.add_topic(high_stock_exalt_to_mirror)
 
-# Start collecting
-thread1.start()
-sleep(5)
-thread2.start()
-sleep(5)
-thread3.start()
-sleep(5)
-thread4.start()
+# Start
+negotiator.start()
