434d3edc88e5baadbf93cb75d533c3c5cea5266b
gustavo-hsm <gustavohsm100@gmail.com>
2020-07-14 23:53:01 -0300

    Added EventType Enum

--- src/EventType.py
+++ src/EventType.py
@@ -2,7 +2,8 @@ from enum import Enum
 
 
 class EventType(Enum):
-    HANDLER_READY = 1
+    HANDLER_READY = 0
+    HANDLER_STARTED = 1
     HANDLER_NEXT = 2
     HANDLER_FINISHED = 3
     HANDLER_ERROR = 4
