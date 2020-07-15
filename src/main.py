2542bc1a9ebab895471ed38d4a803a01606a2349
gustavo-hsm <gustavohsm100@gmail.com>
2020-07-14 23:57:15 -0300

    New testing samples on main file

--- src/main.py
+++ src/main.py
@@ -2,17 +2,22 @@ from ExchangeItem import ExchangeItem
 from RequestNegotiator import RequestNegotiator
 
 # Request negotiator
-negotiator = RequestNegotiator()
+negotiator = RequestNegotiator(total_cycles=5)
 
 # Topics to track
+topics = []
 exalt_to_mirror = ExchangeItem(want='mirror', have='exalted')
 high_stock_exalt_to_mirror = ExchangeItem(want='mirror', have='exalted',
                                           minimum_stock=10,
                                           allow_adjust_minimum_stock=False)
+chaos_to_exalt = ExchangeItem(want='exalted', have='chaos')
+chaos_to_ancient = ExchangeItem(want='ancient-orb', have='chaos',
+                                minimum_stock=16)
 
 # Attach topics
-negotiator.add_topic(exalt_to_mirror)
-negotiator.add_topic(high_stock_exalt_to_mirror)
+topics.extend([exalt_to_mirror, high_stock_exalt_to_mirror, chaos_to_exalt,
+              chaos_to_ancient])
+[negotiator.add_topic(topic) for topic in topics]
 
 # Start
 negotiator.start()
