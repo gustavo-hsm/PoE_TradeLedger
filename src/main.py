0043fe88d135e5285a2524477f89fe5122197ccb
gustavo-hsm <gustavohsm100@gmail.com>
2020-07-20 15:25:09 -0300

    Adjusting main file to new structure

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
-from objects.ExchangeItem import ExchangeItem
+from data_source.ExchangeItem import ExchangeItem
 from data_channel.RequestNegotiator import RequestNegotiator
+from data_sink.TimedExchangeToJSON import TimedExchangeToJSON
 
-# Request negotiator
-negotiator = RequestNegotiator(total_cycles=1)
-
-# Topics to track
 topics = []
+
+# Sample Data sources
 exalt_to_mirror = ExchangeItem(want='mirror', have='exalted')
 high_stock_exalt_to_mirror = ExchangeItem(want='mirror', have='exalted',
                                           minimum_stock=10,
@@ -14,7 +13,13 @@ chaos_to_exalt = ExchangeItem(want='exalted', have='chaos')
 chaos_to_ancient = ExchangeItem(want='ancient-orb', have='chaos',
                                 minimum_stock=16)
 
-# Attach topics
+# Data sink
+exchange_to_json = TimedExchangeToJSON(dir='output/harvest/')
+
+# Data channel
+negotiator = RequestNegotiator(total_cycles=3, data_sink=exchange_to_json)
+
+# Attach sources to channel
 topics.extend([exalt_to_mirror, high_stock_exalt_to_mirror, chaos_to_exalt,
               chaos_to_ancient])
 [negotiator.add_topic(topic) for topic in topics]
