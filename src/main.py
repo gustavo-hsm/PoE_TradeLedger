9ad04a0794a5579e17730ce68d05f6b112ee04e1
gustavo-hsm <gustavohsm100@gmail.com>
2020-09-21 09:34:40 -0300

    Reworked main.py sample code

--- src/main.py
+++ src/main.py
@@ -3,47 +3,39 @@ import logging
 from data_source.ExchangeItem import ExchangeItem
 from data_channel.RequestNegotiator import RequestNegotiator
 from data_channel.ExchangeManager import ExchangeManager
-from data_channel.StashManager import StashManager
 from data_sink.TimedExchangeParser import TimedExchangeParser
-from data_sink.StashParser import StashParser
 
 topics = []
 logging.basicConfig(filename='logs/main.log', filemode='a', level=logging.INFO)
 
 # Sample Data sources
-exalt_to_mirror = ExchangeItem(want='mirror', have='exalted')
-high_stock_exalt_to_mirror = ExchangeItem(want='mirror', have='exalted',
-                                          minimum_stock=10,
-                                          allow_adjust_minimum_stock=False)
-chaos_to_exalt = ExchangeItem(want='exalted', have='chaos')
+chaos_to_exalt = ExchangeItem(want='exalted', have='chaos', league='heist')
+exalt_to_chaos = ExchangeItem(want='chaos', have='exalted', league='heist')
+
 chaos_to_ancient = ExchangeItem(want='ancient-orb', have='chaos',
-                                minimum_stock=16)
+                                league='heist')
+ancient_to_chaos = ExchangeItem(want='chaos', have='ancient-orb',
+                                league='heist')
 
 # Data sink
-exchange_to_json = TimedExchangeParser(dir='output/harvest/',
-                                       prefix='CurrencyWatch')
-
-stash_to_json = StashParser(dir='output/harvest/public-stash/',
-                            id='771388910-784968649-749301602-' +
-                            '847797292-808721043',
-                            prefix='LeagueTracker')
+exchange_to_json = TimedExchangeParser(dir='output/heist/',
+                                       prefix='CurrencyWatch',
+                                       time=180)
 
 # Data channel
 exchange_manager = ExchangeManager(data_sink=exchange_to_json)
-stash_manager = StashManager(data_sink=stash_to_json,
-                             init_id=stash_to_json.get_id())
 
 # Attach sources to channel
-topics.extend([exalt_to_mirror, high_stock_exalt_to_mirror, chaos_to_exalt,
-              chaos_to_ancient])
-[exchange_manager.add_topic(topic) for topic in topics]
+exchange_manager.add_topic(chaos_to_exalt)
+exchange_manager.add_topic(exalt_to_chaos)
+exchange_manager.add_topic(chaos_to_ancient)
+exchange_manager.add_topic(ancient_to_chaos)
 
 # Negotiator to manage Data Channels
-negotiator = RequestNegotiator(total_cycles=3)
+negotiator = RequestNegotiator(total_cycles=10)
 
 # Attach managers to negotiator
 negotiator.add_manager(exchange_manager)
-negotiator.add_manager(stash_manager)
 
 # Start
 negotiator.start()
