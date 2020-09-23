4b85e64d7913a2df40793acd1a14992abcd3d138
gustavo-hsm <gustavohsm100@gmail.com>
2020-09-23 09:15:12 -0300

    Removed unused code

--- src/data_sink/DataSink.py
+++ src/data_sink/DataSink.py
@@ -3,31 +3,6 @@ from threading import Lock
 from objects.Sync_decorator import sync
 
 
-# class DataSink():
-#     lock = Lock()
-
-#     def __init__(self):
-#         self.data = []
-
-#     @sync(lock)
-#     def append_data(self, data):
-#         self.data.append(data)
-
-#     @sync(lock)
-#     def remove_data(self, data):
-#         self.data.remove(data)
-
-#     @sync(lock)
-#     def copy_data(self):
-#         return self.data.copy()
-
-#     def parse(self):
-#         raise NotImplementedError
-
-#     def sink(self):
-#         raise NotImplementedError
-
-
 class DataSink():
     SINK_SUCCESSFUL = 0
     SINK_FAILURE = 1
